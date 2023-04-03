ce50d8c18ac7d82c58caaf78e001e14a82b05d6f
hanochshachar <goralot550@gmail.com>
2023-04-03 04:21:44 +0300

    Add project files.

---
+++ Crud/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;initial Catalog=course; User Id= hanoch; password= 1234; TrustServerCertificate= True");
+        }
+    }
+}

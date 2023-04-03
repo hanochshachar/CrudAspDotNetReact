b4b107a3f172f354ca348c805d66ffe569dc852f
hanochshachar <goralot550@gmail.com>
2023-04-03 04:53:18 +0300

    frontend

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentCrud from './components/StudentCrud';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentCrud/>
+    </div>
+  );
+}
+
+export default App;
